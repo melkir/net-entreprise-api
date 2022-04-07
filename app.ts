055a719ddb0b0cc6457c37e5f4b1e68a22924b0d
Thibault <melkir13@gmail.com>
2022-04-07 22:08:30 +0200

    feat(dev): use serve for local testing

--- app.ts
+++ app.ts
@@ -1,27 +1,19 @@
+import { serve } from "https://deno.land/std@0.134.0/http/server.ts";
 import { cheerio } from "https://deno.land/x/cheerio@1.0.4/mod.ts";
 import { default as format } from "https://deno.land/x/date_fns@v2.22.1/format/index.js";
 import { default as fr } from "https://deno.land/x/date_fns@v2.22.1/locale/fr/index.js";
 import { default as parse } from "https://deno.land/x/date_fns@v2.22.1/parse/index.js";
 
-addEventListener("fetch", async (event) => {
-  const data = await fetchData();
-  const json = JSON.stringify(data);
-  const response = new Response(json, {
-    headers: { "content-type": "application/json; charset=UTF-8" },
-  });
-  // @ts-ignore-next-line
-  event.respondWith(response);
-});
-
 async function fetchData() {
   const res = await fetch(
-    "https://www.net-entreprises.fr/declaration/outils-de-controle-dsn-val/"
+    "https://www.net-entreprises.fr/declaration/outils-de-controle-dsn-val/",
   );
   const html = await res.text();
 
   const $ = cheerio.load(html);
-  const version = $("strong:contains('Version 20')").last().text()
-  const url = $("strong:contains('Dsn-Val pour Linux 64 bits')").last().children().attr("href")
+  const version = $("strong:contains('Version 20')").last().text();
+  const url = $("strong:contains('Dsn-Val pour Linux 64 bits')").last()
+    .children().attr("href");
 
   const [, build, , day, month, year] = version.split(" ");
 
@@ -30,11 +22,18 @@ async function fetchData() {
     `${day}-${month}-${year}`,
     "d-LLLL-y",
     defaultDate,
-    { locale: fr }
+    { locale: fr },
   );
   const date = format(parsedDate, "yyyy-MM-dd", defaultDate);
 
   return { version: build, date, url };
 }
 
-console.log("hello")
+console.log("Listening on http://localhost:8000");
+await serve(async () => {
+  const data = await fetchData();
+  const json = JSON.stringify(data);
+  return new Response(json, {
+    headers: { "content-type": "application/json; charset=UTF-8" },
+  });
+});
